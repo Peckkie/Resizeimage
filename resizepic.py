85513ba22c73205eafda351963e2364833258ad9
gigi0107 <w_yupaporn@kkumail.com>
2019-08-29 10:11:41 +0000

    Chang to 250x250

--- resizepic.py
+++ resizepic.py
@@ -7,5 +7,5 @@ files_list = os.listdir(path)
 
 for f in files_list:
 	print(f)
-	subprocess.call(['ffmpeg','-i',os.path.join(path,f),'-vf','scale=90:90',f])
+	subprocess.call(['ffmpeg','-i',os.path.join(path,f),'-vf','scale=250:250',f])
 
