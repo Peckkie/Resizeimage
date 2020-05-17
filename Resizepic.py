1f6701706292d3ec51799a67a4de2054dc9b4a97
Peckkie <w_yupaporn@kkumail.com>
2020-05-17 12:43:04 +0000

    changing filename

---
+++ Resizepic.py
@@ -0,0 +1,11 @@
+import os 
+import subprocess
+
+path = '/home/gigi01/pics'
+
+files_list = os.listdir(path)
+
+for f in files_list:
+	print(f)
+	subprocess.call(['ffmpeg','-i',os.path.join(path,f),'-vf','scale=250:250',f])
+
